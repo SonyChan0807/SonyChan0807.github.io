0aeed013850b81e0d5a3ccd8959bb4c055840d72
Joey Zenhäusern <joey.zenhausern@epfl.ch>
2017-12-10 17:21:52 +0100

    Clean up a bit and move menu to top

---
+++ js/common.js
@@ -0,0 +1,5 @@
+// Load navigation into page
+$(document).ready(function() {
+    $('#mainNav').load('navigation.html');
+    $('#mainNav').addClass('navbar navbar-expand-lg navbar-dark bg-dark fixed-top');
+});

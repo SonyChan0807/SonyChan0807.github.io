aa0e6949c0a80a73bb63eb1c3dcafbf1f13f5468
Joey Zenhäusern <joey.zenhausern@epfl.ch>
2017-12-16 18:14:04 +0100

    Fix slider issue

--- js/common.js
+++ js/common.js
@@ -15,8 +15,9 @@ $(document).ready(function() {
 sliderYears = [2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017];
 
 // Year slider
+let yearSlider;
 if($('#year-slider').length) {
-    let yearSlider = new Slider("#year-slider", {
+    yearSlider = new Slider("#year-slider", {
       ticks: sliderYears,
       ticks_labels: sliderYears
     });
